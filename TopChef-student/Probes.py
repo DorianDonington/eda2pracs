7c55f81e86dabaae52c0fbbfbf329543583ad0f6
Ferran <feranperez@gmail.com>
2019-04-29 19:51:12 +0200

    First iteration 24/9

---
+++ TopChef-student/Probes.py
@@ -0,0 +1,5 @@
+from top_chef.word_dictionary import WordDictionary
+
+a = WordDictionary()
+a.load_words("TopChef-student/data/words.txt")
+print("succes!")
--- TopChef-student/top_chef/word_dictionary.py
+++ TopChef-student/top_chef/word_dictionary.py
@@ -7,8 +7,33 @@ class WordDictionary:
         self.words = {}
 
     def load_words(self, word_path):
-        # Complete this function
-        pass
+         
+        with open(word_path) as fd:
+            line = fd.readline().strip()
+            title = line.split("\t")
+            
+            if title[0] != "WORD" or title[1] != "VALUE": #Capturamos el error para saber que estamos en el fichero correcto
+                raise WordDictionaryException("There is some error on the database!")
+            else:
+                for line in fd:
+                    stripped = line.strip()
+                    word = stripped.split("\t")
+
+                    if len(word) != 2:   #Error para formato correcto en la lista: con el entero, con que solo hayan dos posiciones
+                        raise WordDictionaryException("There is some error on the database!")
+                    
+                    try:
+                        if not self.exists(word[0]): 
+                            self.add_word(word[0],float(word[1]))
+                        else: 
+                            #Capturamos el error de palabras repetidas por si queremos capturar el error
+                            self.add_word(word[0],float(word[1]))
+
+                    except ValueError: #Capturamos el error de que la segunda palabra no sea un número
+                        raise WordDictionaryException("There is some error on the database!")
+                  
+                    
+                    
 
     def add_word(self, word, value):
         self.words[word] = value
